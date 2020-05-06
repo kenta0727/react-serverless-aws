6c5f8d8edab39eb0d4b9c2b75c57b7e077849fc0
kenta <727shimiken727@ezweb.ne.jp>
2020-05-06 12:06:27 +0900

    error handle

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { withAuthenticator } from 'aws-amplify-react'
+
+function App() {
+  return (
+    <div>App</div>
+  );
+}
+
+export default withAuthenticator(App);
